400529ef8de5b537fac94744ec750b0913dcf139
Jaime Trevino <trevinojaime42@gmail.com>
2021-06-07 15:49:47 -0400

    window width altered

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ function Navbar() {
     const closeMobileMenu =() => setClick(false);
 
     const showButton = () => {
-        if(window.innerWidth <= 960 ){
+        if(window.innerWidth <= 500 ){
             setButton(false)
         } else {
             setButton(true);
