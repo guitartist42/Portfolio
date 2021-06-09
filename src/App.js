bc731af5e76e04c8d871a1cf73bc136e63d49240
Jaime Trevino <trevinojaime42@gmail.com>
2021-06-08 20:50:37 -0400

    design adds

--- src/App.js
+++ src/App.js
@@ -3,18 +3,130 @@ import Navbar from './components/Navbar';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import AboutMe from './components/pages/AboutMe';
-
+import Particles from "react-particles-js";
+  
 function App() {
-  return (
-    <>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route path='/' exact component = {AboutMe}/>
-        </Switch>
-      </Router>
-    </>
-  );
+    return ( <div className="particleBackground">
+        <Router>
+        <Navbar/>
+        {/* <Switch> 
+            <Route path = '/'exact component = { AboutMe }/> 
+        </Switch >  */}
+        </Router>
+
+        <Particles
+        params={{
+          "particles": {
+            "number": {
+              "value": 252,
+              "density": {
+                "enable": true,
+                "value_area": 800
+              }
+            },
+            "color": {
+              "value": "#dcdcdc"
+            },
+            "shape": {
+              "type": "circle",
+              "stroke": {
+                "width": 0,
+                "color": "#000000"
+              },
+              "polygon": {
+                "nb_sides": 5
+              },
+              "image": {
+                "src": "img/github.svg",
+                "width": 100,
+                "height": 100
+              }
+            },
+            "opacity": {
+              "value": 0.5,
+              "random": false,
+              "anim": {
+                "enable": false,
+                "speed": 0.2,
+                "opacity_min": 0.76,
+                "sync": false
+              }
+            },
+            "size": {
+              "value": 3,
+              "random": true,
+              "anim": {
+                "enable": false,
+                "speed": 40,
+                "size_min": 0.1,
+                "sync": false
+              }
+            },
+            "line_linked": {
+              "enable": true,
+              "distance": 150,
+              "color": "#ffffff",
+              "opacity": 0.4,
+              "width": 1
+            },
+            "move": {
+              "enable": true,
+              "speed": 5,
+              "direction": "none",
+              "random": false,
+              "straight": false,
+              "out_mode": "out",
+              "bounce": false,
+              "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+              }
+            }
+          },
+          "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+              "onhover": {
+                "enable": true,
+                "mode": "repulse"
+              },
+              "onclick": {
+                "enable": true,
+                "mode": "push"
+              },
+              "resize": true
+            },
+            "modes": {
+              "grab": {
+                "distance": 400,
+                "line_linked": {
+                  "opacity": 1
+                }
+              },
+              "bubble": {
+                "distance": 400,
+                "size": 40,
+                "duration": 2,
+                "opacity": 8,
+                "speed": 3
+              },
+              "repulse": {
+                "distance": 116,
+                "duration": 0.4
+              },
+              "push": {
+                "particles_nb": 4
+              },
+              "remove": {
+                "particles_nb": 2
+              }
+            }
+          },
+          "retina_detect": true
+        }}/>
+ </div>
+    );
 }
 
 export default App;
--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -5,17 +5,21 @@ function AboutMe () {
     return(
         <>
         <div className = 'Welcome'>
-            Welcome to my Portfolio!
+            <h1>Welcome to my Portfolio! </h1>
         </div>
-        <div className = 'Description'>
+        {/* <div className = 'Description'>
             I recently graduated Duke University with degrees in Computer Science and Linguistics.
             I also have extensive coursework in Mathematics, Music, and the Natural Sciences.
             Some of my favorite hobbies include running, singing, and learning languages. 
-            (Hablo español y 한국어룰 잘한다.)
+            (Hablo español y 한국어를 잘한다.)
             I'm eager to take on new projects! Let's work together.
-        </div>
+        </div> */}
         </>
     )
+}
+
+const styles = {
+
 }
 
 export default AboutMe;
